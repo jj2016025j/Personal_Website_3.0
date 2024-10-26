1f07b24e92cee68ea190a2d9a13ab656cf5183bf
jj2016025j <103946700+jj2016025j@users.noreply.github.com>
2024-10-27 00:50:33 +0800

    路徑改成最原始的

--- static/js/main.js
+++ static/js/main.js
@@ -9,12 +9,6 @@ import { createContactIcons } from './components/contactIcons.js';
 import { renderSkillList } from './components/skills.js';
 import { renderJobList } from './components/jobs.js';
 
-const base = document.createElement('base');
-base.href = window.location.pathname.includes('Personal_Website_3.0')
-    ? '/Personal_Website_3.0/static/'
-    : '/static/';
-document.head.appendChild(base);
-
 document.addEventListener('DOMContentLoaded', () => {
     createHeader();
     renderFeaturedPortfolioList();
