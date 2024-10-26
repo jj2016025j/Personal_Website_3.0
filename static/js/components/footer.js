337ee56fd3a3a2623aa5a999c61f70c23266cab5
jj2016025j <103946700+jj2016025j@users.noreply.github.com>
2024-10-27 01:08:11 +0800

    Update footer.js

--- static/js/components/footer.js
+++ static/js/components/footer.js
@@ -7,12 +7,12 @@ export function createFooter() {
             <ul style="display: flex;">
                 ${createFooterIcon("https://www.instagram.com/lee_871116", "fa-brands fa-instagram")}
                 ${createFooterIcon("https://www.youtube.com/channel/UCdFWtSFizZFpBovsUY7ZKyg", "fa-brands fa-youtube")}
+                ${createFooterIcon("line://ti/p/20161116", "fa-brands fa-line", "LINE")}
                 ${createFooterIcon("https://github.com/jj2016025j", "fa-brands fa-github")}
                 ${createFooterIcon("https://www.pixiv.net/users/73132253", "fa-brands fa-pixiv")}
                 ${createFooterIcon("mailto:ruoxi2002@gmail.com", "fa-brands fa-google")}
                 ${createFooterIcon("mailto:jj2016025j@gmail.com", "fa-solid fa-envelope")}
                 ${createFooterIcon("mailto:0971003199", "fa-solid fa-phone")}
-                ${createFooterIcon("line://ti/p/20161116", "fa-brands fa-line", "LINE")}
                 ${createFooterIcon("https://www.cakeresume.com/jj2016025j", "fa-regular fa-file")}
                 ${createFooterIcon("https://www.linkedin.com/in/%E5%BB%BA%E9%99%9E-%E6%9D%8E-3145931b2/", "fa-brands fa-linkedin")}
             </ul>
