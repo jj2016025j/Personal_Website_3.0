f60988bc3c8265f3d1073aeed17dea4ecb516ab0
jj2016025j <103946700+jj2016025j@users.noreply.github.com>
2024-10-26 22:29:21 +0800

    試著動態調整路徑

--- static/js/components/footer.js
+++ static/js/components/footer.js
@@ -5,12 +5,16 @@ export function createFooter() {
     const footerHTML = `
         <nav>
             <ul style="display: flex;">
-                ${createFooterIcon("#", "fa-brands fa-youtube")}
-                ${createFooterIcon("#", "fa-brands fa-github")}
-                ${createFooterIcon("#", "fa-brands fa-instagram")}
-                ${createFooterIcon("#", "fa-brands fa-linkedin")}
-                ${createFooterIcon("#", "fa-brands fa-google")}
-                ${createFooterIcon("#", "fa-solid fa-envelope")}
+                ${createFooterIcon("https://www.instagram.com/lee_871116", "fa-brands fa-instagram")}
+                ${createFooterIcon("https://www.youtube.com/channel/UCdFWtSFizZFpBovsUY7ZKyg", "fa-brands fa-youtube")}
+                ${createFooterIcon("https://github.com/jj2016025j", "fa-brands fa-github")}
+                ${createFooterIcon("https://www.pixiv.net/users/73132253", "fa-brands fa-pixiv")}
+                ${createFooterIcon("mailto:ruoxi2002@gmail.com", "fa-brands fa-google")}
+                ${createFooterIcon("mailto:jj2016025j@gmail.com", "fa-solid fa-envelope")}
+                ${createFooterIcon("mailto:0971003199", "fa-solid fa-phone")}
+                ${createFooterIcon("https://www.cakeresume.com/jj2016025j", "fa-regular fa-file")}
+                ${createFooterIcon("https://www.linkedin.com/in/%E5%BB%BA%E9%99%9E-%E6%9D%8E-3145931b2/", "fa-brands fa-linkedin")}
+                ${createFooterIcon("https://line.me/ti/p/HhwJIOE8Z1", "fa-brands fa-line", "LINE")}
             </ul>
         </nav>
         <a href="#"><h5>Copyright©</h5></a>
--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,12 @@ import { createContactIcons } from './components/contactIcons.js';
 import { renderSkillList } from './components/skills.js';
 import { renderJobList } from './components/jobs.js';
 
+const base = document.createElement('base');
+base.href = window.location.pathname.includes('Personal_Website_3.0')
+    ? '/Personal_Website_3.0/static/'
+    : '/static/';
+document.head.appendChild(base);
+
 document.addEventListener('DOMContentLoaded', () => {
     createHeader();
     renderFeaturedPortfolioList();
